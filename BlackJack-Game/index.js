953a5b3a6c126416658fd1d5241835e102a1a445
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-06 00:34:11 +0530

    Initial Commit

---
+++ BlackJack-Game/index.js
@@ -0,0 +1,24 @@
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+// Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el")
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    
+    // Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message    
+}
