3e41d6278ae407474b3817ab8655a1bc1d41f7e3
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-06 00:55:30 +0530

    Display the sum

--- BlackJack-Game/index.js
+++ BlackJack-Game/index.js
@@ -8,14 +8,20 @@ let message = ""
 // Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el")
 
+// Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el")
+
 function startGame() {
+    // Render the sum on the page using this format -> "Sum: 14"
+    sumEl.textContent = "Sum " + sum
+
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     
