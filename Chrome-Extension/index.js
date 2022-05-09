96e51172ccff0ee2124e12b9c7f55a0869957da8
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-09 20:07:42 +0530

    Add anchor tag

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -1,6 +1,6 @@
 let myLeads = []
-const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
 // Grab the unordered list and store it in a const variable called ulEl
 const ulEl = document.getElementById("ul-el")
@@ -25,7 +25,9 @@ function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         // Add the item to the listItems variable instead of the ulEl.innerHTML
-        listItems += "<li>" + myLeads[i] + "</li>"
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li><a href='" + myLeads[i] + "' target = '_blank'>" + myLeads[i] + "</a></li>"
     }
     
     // Render the listItems inside the unordered list using ulEl.innerHTML
