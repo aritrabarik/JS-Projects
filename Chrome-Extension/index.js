907374ebd144e7aa72a7f0eaf45fd6625b5d2fd5
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-12 00:55:57 +0530

    Links from localStorage

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -1,4 +1,4 @@
-let myLeads = [] //! this array is converted into string in line 20 as localStorage only supports strings
+let myLinks = [] //! this array is converted into string in line 20 as localStorage only supports strings
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -6,24 +6,33 @@ const inputBtn = document.getElementById("input-btn")
 // Grab the unordered list and store it in a const variable called ulEl
 const ulEl = document.getElementById("ul-el")
 
+// Get the leads from the localStorage
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+
+let linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"))
+console.log(linksFromLocalStorage)
+
 inputBtn.addEventListener('click', function () {
     // Push the value from the inputEl into the myLeads array
     // instead of the hard-coded "www.awesomeleads.com" value
     // Google -> "get value from input field javascript"
-    myLeads.push(inputEl.value)
+    myLinks.push(inputEl.value)
     
     // Clear out the input field
     inputEl.value = ""
 
     // Save the myLeads array to localStorage
     // PS: remember JSON.stringify()
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    localStorage.setItem("myLinks", JSON.stringify(myLinks))
 
     // Call the renderLeads() function
     renderLeads()
 
     // To verify that it works:
-    console.log(JSON.parse(localStorage.getItem("myLeads")))
+    console.log(localStorage.getItem("myLinks"))
+
+    // console.log(JSON.parse(localStorage.getItem("myLeads")))
     //! String is converted to array with JSON.parse
 })
 
@@ -32,15 +41,15 @@ function renderLeads() {
     // Create a variable, listItems, to hold all the HTML for the list items
     // Assign it to an empty string to begin with
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < myLinks.length; i++) {
         // Add the item to the listItems variable instead of the ulEl.innerHTML
         // Wrap the lead in an anchor tag (<a>) inside the <li>
         // Can you make the link open in a new tab?
         
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${myLinks[i]}'>
+                    ${myLinks[i]}
                 </a>
             </li>
         `
